4c68d1128d067a46f23c2c444b5396e63b5f52fe
AxlZurimi <axel_hm@tesch.edu.mx>
2025-12-11 22:06:36 -0600

    Implementacion de metodos de la clase biblioteca

--- src/Clases/Biblioteca.java
+++ src/Clases/Biblioteca.java
@@ -19,4 +19,52 @@ public class Biblioteca {
         libros.add(libro);
         System.out.println("Libro agregado: "+libro.getTitulo());
     }
+    
+    public void registrarUsuario(Usuario usuario){
+        usuarios.add(usuario);
+        System.out.println("Usuario agregado: "+usuario.getNombre());
+    }
+    
+    public void mostrarLibro(){
+        if (libros.isEmpty()){
+            System.out.println("No hay libros en la biblioteca");
+        } else {
+            for(Libro libro : libros){
+                libro.mostrarInfo();
+            }
+        }
+    }
+    
+    public void prestarLibro(int idLibro){
+        Libro libro = buscarLibroPorId(idLibro);
+        if(libro == null){
+            System.out.println("Error: el libro con ID "+idLibro+ " no existe");
+        } else if (!libro.isDisponible()){
+            System.out.println("El libro: '"+libro.getTitulo()+ "' no esta disponible.");
+        } else {
+            libro.setDisponible(false);
+            System.out.println("Se ha prestado el libro: "+libro.getTitulo());
+        }
+    }
+    
+    public void devolverLibro(int idLibro){
+        Libro libro = buscarLibroPorId(idLibro);
+        if(libro == null){
+            System.out.println("Error: el libro con ID "+idLibro+ " no existe");
+        } else if (!libro.isDisponible()){
+            System.out.println("El libro: '"+libro.getTitulo()+ "' no esta disponible");
+        } else {
+            libro.setDisponible(true);
+            System.out.println("Se ha prestado el libro: "+libro.getTitulo());
+        }
+    }
+    
+    private Libro buscarLibroPorId(int idLibro){
+        for(Libro libro : libros){
+            if(libro.getId() == idLibro){
+                return libro;
+            }
+        }
+        return null;
+    }
 }
